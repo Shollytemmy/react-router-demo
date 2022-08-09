7e9f06606690ee9fb33331f0fb490e4bf437206a
Bunyameen <bunyameen@gmail.com>
2022-08-09 16:02:36 +0100

    added Go to previous page functionalities

--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const UserDetails = () => {
+    const navigate = useNavigate()
     const {userID} = useParams()
     console.log(userID)
     return(
         <div>
             <h1>User {userID} Details Dashboard</h1>
+            <button onClick={() => navigate(-1)}>Go Back</button>
         </div>
     )
 }
