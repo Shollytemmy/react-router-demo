868e50efa0658394aa07bdb8ccd1de32a8d24a1d
Bunyameen <bunyameen@gmail.com>
2022-08-09 15:37:47 +0100

    Logics for Showing individual user details added

--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export const UserDetails = () => {
+    const {userID} = useParams()
+    console.log(userID)
     return(
         <div>
-            <h1>UserDetails</h1>
+            <h1>User {userID} Details Dashboard</h1>
         </div>
     )
 }
--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 export const Users = () => {
+    // const {userID} = useParams()
+    const usersId = [1, 2, 3, 6, 9, 56]
   return (
-    <div>
-        <h1>user 1</h1>
-        <h1>user 2</h1>
-        <h1>user 3</h1>
+    <div style={{border: '1px solid red'}}>
+        {
+            usersId.map((id) => {
+                return(
+                     <Link to={`/users/${id}`} key={id}>
+                            <h1>user {id}</h1>
+                            
+                        </Link>
+                )
+            })
+        }
+
+     
     </div>
   )
 }
