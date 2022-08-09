62333297baf7a98206c4446094161a43bd6960d6
Bunyameen <bunyameen@gmail.com>
2022-08-09 15:51:23 +0100

    first final stage

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useNavigate} from 'react-router-dom'
 
 export const Users = () => {
     // const {userID} = useParams()
+    const navigate = useNavigate()
     const usersId = [1, 2, 3, 6, 9, 56]
   return (
-    <div style={{border: '1px solid red'}}>
+    <div style={{border: '0px solid red'}}>
         {
             usersId.map((id) => {
                 return(
@@ -21,3 +22,14 @@ export const Users = () => {
     </div>
   )
 }
+
+
+/**
+ * 
+ * [
+ * {
+ * id: 1,
+ * name: ""
+ * }
+ * ]
+ */
